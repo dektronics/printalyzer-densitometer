9859cb02525d7b8eb93a5d6fbfd8e5a91288883c
Derek Konigsberg <dkonigsberg@dektronics.com>
2021-12-09 10:13:28 -0800

    Reduce GPIO pin speed for I2C and USART
    These can be set to LOW without any problems.
    SPI still likely needs to be at VERY_HIGH, but will continue to
    evaluate.
    
    Refs #23

--- software/firmware/src/system/stm32l0xx_hal_msp.c
+++ software/firmware/src/system/stm32l0xx_hal_msp.c
@@ -103,7 +103,7 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
         GPIO_InitStruct.Pin = GPIO_PIN_6 | GPIO_PIN_7;
         GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
         GPIO_InitStruct.Pull = GPIO_NOPULL;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
         GPIO_InitStruct.Alternate = GPIO_AF1_I2C1;
         HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
@@ -249,7 +249,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
         GPIO_InitStruct.Pin = GPIO_PIN_9 | GPIO_PIN_10;
         GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
         GPIO_InitStruct.Pull = GPIO_NOPULL;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
         GPIO_InitStruct.Alternate = GPIO_AF4_USART1;
         HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
     }
