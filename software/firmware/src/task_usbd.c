2e52ee1b6f2e4abc328bdf150354c4ede1145a38
Derek Konigsberg <dkonigsberg@dektronics.com>
2023-06-01 12:57:40 -0700

    Rename callback functions due to library changes

--- software/firmware/src/task_usbd.c
+++ software/firmware/src/task_usbd.c
@@ -135,7 +135,7 @@ void tud_resume_cb()
  * Application can use this to send the next report.
  * Note: For composite reports, report[0] is report ID.
  */
-void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint8_t len)
+void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint16_t len)
 {
     (void) instance;
     (void) len;
@@ -155,9 +155,9 @@ void tud_hid_report_complete_cb(uint8_t instance, uint8_t const* report, uint8_t
  * Application must fill buffer report's content and return its length.
  * Returning zero will cause the stack to STALL request.
  */
-uint16_t tud_hid_get_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen)
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen)
 {
-    (void) itf;
+    (void) instance;
     (void) report_id;
     (void) report_type;
     (void) buffer;
@@ -173,9 +173,9 @@ uint16_t tud_hid_get_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t
  * Invoked when receiving a SET_REPORT control request or received data
  * on the OUT endpoint ( Report ID = 0, Type = 0)
  */
-void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize)
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize)
 {
-    (void) itf;
+    (void) instance;
     (void) report_id;
     (void) report_type;
     (void) buffer;
